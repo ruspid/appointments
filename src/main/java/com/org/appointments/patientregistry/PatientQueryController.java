b04bf707f256a34582eb8e642a336449758594b0
ruspid <pidgaynyj@gmail.com>
2020-05-06 13:47:25 +0200

    pagination params setup in controller, itegration tests for patient query controller

--- src/main/java/com/org/appointments/patientregistry/PatientQueryController.java
+++ src/main/java/com/org/appointments/patientregistry/PatientQueryController.java
@@ -4,13 +4,12 @@ package com.org.appointments.patientregistry;
 import com.org.appointments.patientregistry.dto.PatientDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,13 +17,23 @@ class PatientQueryController {
 
     private final PatientQueryRepository patientQueryRepository;
 
-    @GetMapping("/patients")
-    ResponseEntity<Page<PatientDto>> readAllPatients(@RequestParam(value = "q", required = false) Optional<String> q, Pageable pageable) {
-        Page<PatientDto> patients = q.map(p -> patientQueryRepository.searchPatients(p, pageable))
-                .orElseGet(() -> patientQueryRepository.readAllPatients(pageable));
-        return ResponseEntity.ok(patients);
+    private Pageable getPageable(Integer perPage, Integer pageNum) {
+        int page = pageNum != null ? pageNum : 1;
+        int size = perPage != null ? perPage : 20;
+        return PageRequest.of(page, size);
     }
 
+    @GetMapping("/patients")
+    ResponseEntity<Page<PatientDto>> readAllPatients(
+            @RequestParam(value = "q", required = false) String q,
+            @RequestParam(value = "perPage", required = false) Integer perPage,
+            @RequestParam(value = "pageNum", required = false) Integer pageNum) {
+        Pageable pageable = getPageable(perPage, pageNum);
+        Page<PatientDto> patients = q == null ?
+                patientQueryRepository.readAllPatients(pageable)
+                : patientQueryRepository.searchPatients(q, pageable);
+        return ResponseEntity.ok(patients);
+    }
 
 
 }
--- src/test/java/com/org/appointments/patientregistry/PatientQueryControllerITCase.java
+++ src/test/java/com/org/appointments/patientregistry/PatientQueryControllerITCase.java
@@ -1,29 +1,56 @@
 package com.org.appointments.patientregistry;
 
+import com.org.appointments.patientregistry.dto.PatientRegistrationDto;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class PatientQueryControllerITCase {
 
     @Autowired
     MockMvc mvc;
 
+    @Autowired
+    PatientFacade patientFacade;
+
+    @BeforeAll
+    public void initTestData() {
+        patientFacade.registerPatient(new PatientRegistrationDto("John", "Wick", "NY"));
+        patientFacade.registerPatient(new PatientRegistrationDto("Wiz", "Cudi", "LA"));
+        patientFacade.registerPatient(new PatientRegistrationDto("Joyner", "Lucas", "AT"));
+    }
+
     @Test
     void shouldReturnPageOffAllPatients() throws Exception {
+        //given - testData
+        //when
         mvc.perform(MockMvcRequestBuilders.get("/patients"))
-
+                //then
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", notNullValue()));
+                .andExpect(jsonPath("$.content", hasSize(3)));
     }
+
+    @Test
+    void shouldReturnPageOfMatchingPatients() throws Exception {
+        //given - testData
+        //when
+        mvc.perform(MockMvcRequestBuilders.get("/patients").param("q", "jo"))
+                //then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content", hasSize(2)));
+    }
+
 
 }
